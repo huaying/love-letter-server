ec217f01d3ea799491b0e30278cddf2c3e29a35b
huaying <royal3501@gmail.com>
2018-01-15 14:09:34 -0800

    setup babel and eslint

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import morgan from 'morgan';
+import SocketIO from 'socket.io';
+
+const app = express();
+
+// logger
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/client.html');
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+	console.log(`Started on port ${server.address().port}`);
+});
+
+const io = SocketIO(server);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
